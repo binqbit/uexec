9f73bcfc303ffe637559f15412c64354920aa92d
binqbit <binqbit@gmail.com>
2024-01-20 00:54:11 +0200

    optimized script

--- vexec.bat
+++ vexec.bat
@@ -1,14 +1,15 @@
 @echo off
 color b
-cd /d %~dp0
+cd /d "%~dp0"
 
 if "%1" == "--key" (
-    call python .\src\main.py %*
+    call python ".\src\main.py" %*
 ) else if "%1" == "--admin" (
     setlocal EnableDelayedExpansion
-    set all_args=%*
-    set args=!all_args:~8!
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~dp0\vexec.bat !args!' -Verb RunAs"
+    set "args=%*"
+    set "args=!args:~8!"
+    powershell -Command "Start-Process cmd -ArgumentList '/c \"%~dp0\vexec.bat\" !args!' -Verb RunAs"
+    endlocal
 ) else (
-    call python .\src\main.py %*
+    call python ".\src\main.py" %*
 )
